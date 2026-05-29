dc4b00df6e4639e2d53eb3a2dad5f61c04046e20
begnami <caiobegnamii@gmail.com>
2026-05-29 13:47:48 -0300

    Criei parcialmente o sistema de menu com as opções de adicionar produto e de listar os produtos. Também criei a matriz de produtos, porém ainda não possui nada.

---
+++ sces.py
@@ -0,0 +1,31 @@
+produtos = [
+    [],
+]
+
+def adicionarProduto():
+    novoProduto = input("Adicione os dados do novo produto (ex: ): ")
+    produtos.append(novoProduto)
+    print("Produto adicionado com sucesso! 📦")
+
+def listarProdutos():
+    for lista in produtos:
+        print(f"Os produtos diponíveis são: {lista}")
+
+print("\nSistema de Controle de Estoque Simplificado (SCES)")
+print("\nBem vindo ao menu de opções. Por favor selecione uma opção: ")
+while True:
+    print("\n1- Adicionar produto | 2- Listar todos os produtos | 3- Buscar Produto por ID | 4- Atualizar estoque | 5- Sair")
+    opçao = input("Escolha: ")
+    if (opçao == "1"):
+        adicionarProduto()
+    elif (opçao == "2"):
+        listarProdutos()
+    # elif(opçao == "3"):
+    #     buscarProduto()
+    # elif(opçao == "4"):
+    #     atualizarEstoque()
+    # elif(opçao == "5"):
+    #     print("Menu encerrado!")
+    #     break
+    # else:
+    #     print("Esse caractere não corresponde a nenhuma das opções, tente novamente.")
